7faf83d89c6719206e0a0422c3ff4c44cb9b88f5
Konstantin Simeonov <kon.simeonov@gmail.com>
2016-09-26 11:28:26 +0300

    Added tests for invalid string values

--- schema-validator/tests/invalid.js
+++ schema-validator/tests/invalid.js
@@ -118,4 +118,85 @@ describe('Should return errors', () => {
         })
     })
 
+    describe('should return errors for invalid string property', () => {
+
+        it('should throw error when string value expected but value of other type is provided', () => {
+            const schema = {
+                name: {
+                    __type: 'schema',
+                }
+            }
+
+            const errors = validate(schema, { name: [] })
+
+            expect(errors.length).to.equal(1)
+        })
+
+        it('should throw error when string value required but property not present', () => {
+            const schema = {
+                name: {
+                    __type: 'string',
+                    required: true
+                }
+            }
+
+            const errors = validate(schema, { name: null })
+
+            expect(errors.length).to.equal(1)
+        })
+
+        it('should throw error when string value length is under the minimum length', () => {
+            const schema = {
+                name: {
+                    __type: 'string',
+                    minlength: 5
+                }
+            }
+
+            const errors = validate(schema, { name: 'oops' })
+
+            expect(errors.length).to.equal(1)
+        })
+
+        it('should throw error when string value length is above the maximum length', () => {
+            const schema = {
+                name: {
+                    __type: 'string',
+                    maxlength: 5
+                }
+            }
+
+            const errors = validate(schema, { name: 'baddy baddity bad' })
+
+            expect(errors.length).to.equal(1)
+        })
+
+        it('should throw error when string value doesn`t match the provided regexp pattern', () => {
+            const schema = {
+                serialNumber: {
+                    __type: 'string',
+                    required: true,
+                    pattern: /[0-9]{8}/
+                }
+            }
+
+            const errors = validate(schema, { serialNumber: '1234567' })
+
+            expect(errors.length).to.equal(1)
+        })
+
+        it('should return error when string value doesn`t fullfill the predicate', () => {
+            const schema = {
+                description: {
+                    __type: 'string',
+                    predicate: value => value.indexOf('object-oriented') === -1
+                }
+            }
+
+            const errors = validate(schema, { description: 'C#, Java and C++ are object-oriented languages' })
+
+            expect(errors.length).to.equal(1)
+        })
+    })
+
 })
