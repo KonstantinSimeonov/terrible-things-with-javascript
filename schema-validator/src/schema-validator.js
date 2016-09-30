076a7fac0333e4e7b06eb1a681bb159328686c25
KonstantinSimeonov <kon.simeonov@gmail.com>
2016-09-30 18:02:28 +0300

    Added custom error type with more details feedback

--- schema-validator/src/schema-validator.js
+++ schema-validator/src/schema-validator.js
@@ -1,49 +1,59 @@
-'use strict';
+'use strict'
 
-function validateLength(constraints, value, errors) {
+function ValidationError(type, propertyName, message) {
+
+    Error.captureStackTrace(this, ValidationError)
+    this.message = message
+    this.type = type
+    this.propertyName = propertyName
+}
+
+ValidationError.prototype = Object.create(Error.prototype)
+
+function validateLength(constraints, value, errors, propName) {
     if (constraints.minlength && constraints.minlength > value.length) {
-        errors.push(new Error(`Expected length to be at least ${constraints.minlength} but got ${value.length}`))
+        errors.push(new ValidationError('tooShortError', propName, `Expected length to be at least ${constraints.minlength} but got ${value.length}`))
     }
 
     if (constraints.maxlength && constraints.maxlength <= value.length) {
-        errors.push(new Error(`Expected length to be less than ${constraints.maxlength} but got ${value.length}`))
+        errors.push(new ValidationError('tooLongError', propName, `Expected length to be less than ${constraints.maxlength} but got ${value.length}`))
     }
 }
 
-function validatePredicate(constraints, value, errors) {
+function validatePredicate(constraints, value, errors, propName) {
     if (constraints.predicate && !constraints.predicate(value)) {
-        errors.push(new Error('Failed predicate validation'))
+        errors.push(new ValidationError('predicateError', propName,'Failed predicate validation'))
     }
 }
 
-function validateRange(constraints, value, errors) {
+function validateRange(constraints, value, errors, propName) {
     if (constraints.max && constraints.max <= value) {
-        errors.push(new Error(`Expected value to be less than ${constraints.max} but got ${value}`))
+        errors.push(new ValidationError('tooLargeError', propName, `Expected value to be less than ${constraints.max} but got ${value}`))
     }
 
     if (constraints.min && constraints.min > value) {
-        errors.push(new Error(`Expected value to be at least ${constraints.min} but got ${value}`))
+        errors.push(new ValidationError('tooSmallError', propName,`Expected value to be at least ${constraints.min} but got ${value}`))
     }
 }
 
-function validateNull(constraints, value, errors) {
+function validateNull(constraints, value, errors, propName) {
     const isRequired = 'required' in constraints && constraints.required
 
     if (isRequired && value == undefined) {
-        errors.push(new Error(`Value cannot be null or undefined`))
+        errors.push(new ValidationError('nullOrUndefinedError', propName, `Value cannot be null or undefined`))
     }
 }
 
-function validateType(constraints, value, errors) {
+function validateType(constraints, value, errors, propName) {
     const type = constraints.__type,
         isRequired = 'required' in constraints && constraints.required
 
     if (isRequired && typeof value !== type) {
-        errors.push(new Error(`Expected value to be of type ${type} but got ${typeof value}`))
+        errors.push(new ValidationError('typeError', propName,`Expected value to be of type ${type} but got ${typeof value}`))
     }
 }
 
-function validateString(constraints, value) {
+function validateString(constraints, value, propName) {
     const errors = []
 
     validateNull(constraints, value, errors)
@@ -51,7 +61,7 @@ function validateString(constraints, value) {
     validateLength(constraints, value, errors)
 
     if (constraints.pattern && value.match(constraints.pattern).shift() !== value) {
-        errors.push(new Error('Expected to match pattern but did not'))
+        errors.push(new ValidationError('regexpMismatchError', propName, 'Expected to match pattern but did not'))
     }
 
     validatePredicate(constraints, value, errors)
@@ -59,19 +69,19 @@ function validateString(constraints, value) {
     return errors
 }
 
-function validateNumber(constraints, value) {
+function validateNumber(constraints, value, propName) {
     const errors = []
 
-    validateNull(constraints, value, errors)
+    validateNull(constraints, value, errors, propName)
 
-    validateRange(constraints, value, errors)
+    validateRange(constraints, value, errors, propName)
 
     if (constraints.integer && (value | 0) !== value) {
-        errors.push(new Error(`Expected integer value but got ${value}`))
+        errors.push(new ValidationError('notIntegerError', propName,`Expected integer value but got ${value}`))
     }
 
     if (constraints.required && !constraints.allowNaN && isNaN(value)) {
-        errors.push(new Error(`Expected a number but got ${value}`))
+        errors.push(new ValidationError('nullOrUndefinedError', propName, `Expected a number but got ${value}`))
     }
 
     validatePredicate(constraints, value, errors)
@@ -79,19 +89,19 @@ function validateNumber(constraints, value) {
     return errors
 }
 
-function validateBoolean(constraints, value) {
+function validateBoolean(constraints, value, propName) {
     const errors = []
 
-    validateNull(constraints, value, errors)
+    validateNull(constraints, value, errors, propName)
 
     return errors
 }
 
-function validateEnum(constraints, value) {
+function validateEnum(constraints, value, propName) {
     const errors = []
 
     if (!constraints.allowed.some(v => v === value)) {
-        errors.push(new Error(`Expected one of ${constraints.allowed} but got ${value}`))
+        errors.push(new ValidationError('invalidEnumError', propName, `Expected one of ${constraints.allowed} but got ${value}`))
     }
 
     return errors
@@ -133,7 +143,7 @@ function createValidateFunction(validationFunctions) {
                 validateProperty = validationFunctions[subSchema.__type]
 
             if (validateProperty) {
-                const propagatedErrors = validateProperty(subSchema, propertyToValidate)
+                const propagatedErrors = validateProperty(subSchema, propertyToValidate, propName)
                 errors.push(...propagatedErrors)
             }
         }
