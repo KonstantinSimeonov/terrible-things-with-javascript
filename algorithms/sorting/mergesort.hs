e2681cc545ea1c459523eb44aae5af54557bfb66
KonstantinSimeonov <kon.simeonov@gmail.com>
2016-11-02 18:54:59 +0200

    Improved haskell mergesort

--- algorithms/sorting/mergesort.hs
+++ algorithms/sorting/mergesort.hs
@@ -1,26 +1,21 @@
 mergesort :: Ord a => [a] -> [a]
 
-mergesort = head . msort . single
+mergesort = headOrEmpty . msort . map (\x -> [x])
     where
         -- merge two sorted lists
         merge :: Ord a => [a] -> [a] -> [a]
-
         merge [] ys = ys
         merge xs [] = xs
         merge (x:xs) (y:ys)
                 | x < y     = x:merge xs (y:ys)
                 | otherwise = y:merge (x:xs) ys
-        
-        -- list -> list of singletons
-        single :: [a] -> [[a]]
 
-        single [] = []
-        single (x:xs) = [x]:single xs
+        headOrEmpty :: [[a]] -> [a]
+        headOrEmpty [x] = x
+        headOrEmpty [] = []
 
         -- merge sort a list of singletons using merge
         msort :: Ord a => [[a]] -> [[a]]
-
         msort [] = []
         msort [x] = [x]
         msort (x:x':xs) = msort ((merge x x'):xs)
-
