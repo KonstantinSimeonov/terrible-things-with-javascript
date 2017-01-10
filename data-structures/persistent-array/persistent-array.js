c79714c4d7c1a694bd86adc9bbcc3c0111d16ad8
Konstantin Simeonov <kon.simeonov@gmail.com>
2017-01-10 22:29:50 +0200

    Forgot to save :D

--- data-structures/persistent-array/persistent-array.js
+++ data-structures/persistent-array/persistent-array.js
@@ -73,11 +73,3 @@ const PersistentArray = (function () {
 if(typeof module !== 'undefined') {
     module.exports = PersistentArray;
 }
-
-// let tree = PersistentArray(32).update(0, 0).update(1, 'uho');
-
-// let tree2 = tree.update(18, 'pzo');
-// tree = tree.update(19, {});
-
-// Array.from({length: 31}).map((_, i) => i).forEach(i => console.log(tree.at(i), i));
-// [0, 1, 18, 19].forEach(i => console.log(//i, tree.at(i), tree2.at(i)));
