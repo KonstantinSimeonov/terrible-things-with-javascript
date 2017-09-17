58f720504f0beb2b4421c6415aafb525e4d3d39c
Konstantin Simeonov <kon.simeonov@protonmail.com>
2017-09-17 13:25:03 +0300

    Much simpler promise await-all implementation

--- promises/await-all.js
+++ promises/await-all.js
@@ -1,55 +1,16 @@
-/**
- * Await all promises to resolve/reject, even if some of them reject.
- * 
- * @param {any} promises - Promises to await.
- * @returns Promise<any>
- */
+'use strict';
+
+const wrap = key => value => ({ [key]: value });
+const onResolve = wrap('value');
+const onReject = wrap('error');
+const transformToResolved = promise => promise.then(onResolve, onReject);
+
 function awaitAll(promises) {
-	const { length } = promises;
-	const result = [];
-	const errorIndexes = new Set;
-	let completed = 0;
-
-	return new Promise((resolve, reject) => {
-		const onPromiseComplete = () => {
-			if (++completed < length) {
-				return;
-			}
-
-			if (errorIndexes.size) {
-				reject(
-					result.map((value, index) => {
-						if (errorIndexes.has(index)) {
-							return { error: value, state: 'rejected' };
-						}
-
-						return { value, state: 'resolved' };
-					})
-				);
-			} else {
-				resolve(result);
-			}
-		};
-
-		for (let i = 0; i < length; ++i) {
-			promises[i]
-				.then(value => {
-					result[i] = value;
-
-					onPromiseComplete();
-				})
-				.catch(error => {
-					result[i] = error;
-					errorIndexes.add(i);
-
-					onPromiseComplete();
-				});
-		}
-	});
+	return Promise.all(promises.map(transformToResolved));
 }
 
 awaitAll([
 	Promise.resolve('zdr'),
 	Promise.reject(10),
 	new Promise(resolve => setTimeout(() => resolve(50), 2000))
-]).then(stuff => console.log('success', stuff), stuff => console.log('fail', stuff))
+]).then(results => console.log('promises finished', results), error => console.log('unexpected error', error))
